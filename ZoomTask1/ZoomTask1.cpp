322e28c868cb28c29bdcce9318728459d2a120e7
Kigaroth <hp_20@hotmail.com>
2020-09-01 14:34:58 +0200

    Testing

--- ZoomTask1/ZoomTask1.cpp
+++ ZoomTask1/ZoomTask1.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <string>
+#include <conio.h>
 
 int age;
+int a = 20;
 
 int main() {
 	std::cout << "How old are you?";
 	std::cin >> age;
-	
-	if
+	std::cout << "Your age is: \n" << age; << "\n"
+
+		if (age >= a) {
+			std::cout << "You are an adult.\n";
+		}
+		else {
+			std::cout << "You are a teenager.\n";
+		}
 }
